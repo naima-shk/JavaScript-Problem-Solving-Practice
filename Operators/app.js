2c1f0a85f94f42025f4693e24886bd0a988e952e
naima-shk <naimashaikh550@gmail.com>
2021-05-25 15:47:04 +0500

    'Logical-assignment-arithmetic-increment-operators-implemented'

---
+++ Operators/app.js
@@ -0,0 +1,67 @@
+var x = 5;
+var  y = 10;
+var  z = 15;
+
+//x + y;
+console.log(x+y);
+
+//y - x
+console.log(y - x);
+
+//x * y
+console.log(x * y);
+
+//y / x; 
+console.log(y / x);
+
+//x % 2; 
+console.log(x % 2);
+
+//x++; 
+console.log(x++);
+
+//x--
+console.log(x--);
+
+
+var a = 5;
+var  b = 10;
+var c = "5";
+
+var x = a;
+
+//a == c;
+console.log(a == c);
+
+//a === c;
+console.log(a === c);
+
+//a == x; 
+console.log(a == x);
+
+//a != b; 
+console.log(a != b);
+
+//a > b;
+console.log(a > b);
+
+//a < b;
+console.log(a < b);
+
+//a >= b; 
+console.log(a >= b);
+
+//a <= b; 
+console.log(a <= b);
+
+//a >= c; 
+console.log(a >= c);
+
+//a <= c;
+console.log(a <= c);
+
+// Write a program that uses logical and assignment operators.
+ console.log(1>3 && 5<6);
+
+ // Write a program that converts degree to fahrenheit and fahrenheit to degrees.
+ 
