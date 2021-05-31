a11e72dcc0b369e2ee539684f97ada9ec5d26455
naima-shk <naimashaikh550@gmail.com>
2021-05-31 14:44:08 +0500

    'check-data-types-of-variables'

---
+++ DataTypes/app.js
@@ -0,0 +1,26 @@
+//Assign an integer value to your variable.
+let num=34;
+console.log(typeof num);
+//the answer is number
+
+//Assign a decimal value to your variable.
+let dec=3.6;
+console.log(typeof dec);
+//the answer is number
+
+
+//Assign a boolean value to your variable.
+let bool= false;
+console.log(typeof bool);
+//the answer is boolean
+
+//Assign any string to your variable. Remember to put quotation marks. Check its data type from the console. Now remove the quotation marks and see if it is still a string or do you see an error?
+let name ='naima';
+console.log(typeof name);
+//the answer is string
+//i see an error after removing the quotation mark 
+
+//Assign your variable a value of null. Check its type in the console.
+let assign= null;
+console.log(typeof assign);
+//the answer is object
