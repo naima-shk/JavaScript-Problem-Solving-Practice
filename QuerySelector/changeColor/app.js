35a2a737fd1c48e8e0c73a72c967b0c4780986b3
naima-shk <naimashaikh550@gmail.com>
2021-05-31 13:56:54 +0500

    'change-the-bgcolor-using-query-selector'

---
+++ QuerySelector/changeColor/app.js
@@ -0,0 +1,7 @@
+function changeColor() {
+    docBody = document.querySelector("body");
+     //Get all the p elements that are descendants of the body
+     myBodyElements = docBody.querySelector("p");
+     myp1 = myBodyElements;
+     myp1.style.background = "blue";
+   }
