e112e8f6b3d308e1f84e80538e0bafbc1fe533c6
naima-shk <naimashaikh550@gmail.com>
2021-06-02 13:31:08 +0500

    'check-variable-problem-JS-coding-challenge-2'

---
+++ Variables/checkVariable/app.js
@@ -0,0 +1,5 @@
+// 3-Create a function called myFunc and declare a variable name inside the body of that function. In the console check the value of the name and see what console prints.
+function myFunc(){
+    let number;
+    console.log(number);
+}
