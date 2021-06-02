16059482abfc6e60c0e92ed13065ae5af5a20435
naima-shk <naimashaikh550@gmail.com>
2021-06-02 13:26:57 +0500

    'arithmetic-operators-problem-JS-coding-challenge-2'

---
+++ Operators/Arithmetic-Operators/app.js
@@ -0,0 +1,10 @@
+//Create two variables and alert their sum, multiplication, and divide var1 by var2 and see if you get the desired output or not. If not then see why and how to get it.
+
+
+
+let a = 4;
+let b= 5;
+
+console.log(alert(a+b));
+console.log(alert(a * b));
+console.log(alert(a/b));
