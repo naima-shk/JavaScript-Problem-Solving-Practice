fc9abb3f211824ef0d80a167ea1d35d326db5843
naima-shk <naimashaikh550@gmail.com>
2021-06-02 13:37:31 +0500

    'variable-with-data-type-problem-JS-coding-challenge-1'

---
+++ Variables/variables-with-data-type/app.js
@@ -0,0 +1,44 @@
+//Variables
+
+//numbers
+let cash= 500;
+console.log(cash);
+
+//string
+let myVariable = 'Bob';
+   myVariable='charles';
+console.log(myVariable);
+
+//boolean
+let isRaining = true;
+ isRaining=false;
+console.log(isRaining);
+
+/*constant */
+//numbers
+const month=12;
+ console.log(month);
+
+//string
+const myName='naima';
+console.log(myName);
+
+//boolean
+const num1= 10;
+const num2=9;
+if (num1>num2) {
+    console.log("true");
+  } else {
+    console.log("false");
+  }
+
+  //Add variables
+  let a=4;
+  let b=12;
+  let c= a+b;
+  console.log('The sum of ' + a + ' and ' + b + ' is: ' + c);
+
+// Write a program that takes two variables and prints the sum of it.
+let x=12;
+let y=20;
+console.log(x+y);
