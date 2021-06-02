63685099cf9cf4ba2a0c093d38dbb798af99e580
naima-shk <naimashaikh550@gmail.com>
2021-06-02 13:28:58 +0500

    'Increment-operators-problem-JS-coding-challenge-2'

---
+++ Operators/Increment-Operators/app.js
@@ -0,0 +1,10 @@
+//Create a variable and display it on the webpage. Add a button on the webpage. Create a function that runs on every click of the button. Whenever the function is executed the value of the variable will be increased by 5.
+let num = 999;
+document.write(num); 
+
+let  n = 0;
+function increment(){
+    n=n+5;
+    console.log(n);
+    return n;
+}
