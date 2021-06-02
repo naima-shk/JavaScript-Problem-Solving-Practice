75b951232e082878a6c474f14ce38c433ab354fc
naima-shk <naimashaikh550@gmail.com>
2021-06-02 13:32:44 +0500

    'check-variable-scope-problem-JS-coding-challenge-2'

---
+++ Variables/checkVariableScope/app.js
@@ -0,0 +1,10 @@
+// 1- Create a variable my_num and assign it a number. Convert my_num into a string using a toString() method.
+/*var my_num = 45;
+var num= my_num.toString();
+*/
+// 2-Create a variable myString and assign it a value of "2.3". Now create 2 more new variables. One integer and assign it a value of parsInt(myString) and another float and assign it a value of parseFloat(myString). Check the value and type of integer and float in the console.
+/*let myString ='2.3';
+let myNumber= parseInt(myString);
+let myFloat = parseFloat(myString);
+*/
+
