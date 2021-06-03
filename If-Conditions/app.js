fcdbc78382dd206d9de3575a38574a44bd16bf84
naima-shk <naimashaikh550@gmail.com>
2021-06-03 15:35:35 +0500

    'JS-challenge-3'

---
+++ If-Conditions/app.js
@@ -0,0 +1,7 @@
+//Write the if else statement by using the condition that if student has gained marks more than or equal to 50 then student is pass else student is fail.
+const passingMarks= 55;
+if (passingMarks >= 50){
+    alert('student is pass')
+} else {
+alert('student is fail')
+}
