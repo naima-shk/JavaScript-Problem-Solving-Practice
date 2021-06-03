5cfb8b23f50fedcc2c29a53488ae9c60465cc8a9
naima-shk <naimashaikh550@gmail.com>
2021-06-03 15:33:59 +0500

    'JS-challenge-3'

---
+++ Operators/comparison-operator/app.js
@@ -0,0 +1,14 @@
+//List down the comparison operators. What happens if you write 4<=5?
+/* 
+1-equal to ==
+2-equal value and equal type ===
+3-!=	not equal
+4-!==	not equal value or not equal type
+5- >	greater than
+6-  <	less than
+7- >=	greater than or equal to
+8- <=	less than or equal to
+
+*/
+let a= 4<=5;
+ console.log(4<=5);
