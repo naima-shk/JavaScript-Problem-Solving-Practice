f3edf876177f351e4215c8ca56bc7e918fb21741
naima-shk <naimashaikh550@gmail.com>
2021-06-04 14:15:28 +0500

    'Loops-implemented-JS-coding-challenge-4'

--- Loops/WhileLoop/app.js
+++ Loops/WhileLoop/app.js
@@ -1,6 +1,5 @@
 let a=1;
 while(a<=10){
-    
     a=a+1;
     console.log('naima');
 }
--- Loops/for-loops/app.js
+++ Loops/for-loops/app.js
@@ -1,7 +1,7 @@
-/*for (let i=0; i<=50; i++){
+for (let i=0; i<=50; i++){
     console.log('clap');
 }
-*/
+
 
 for (let i=0; i<=199; i++){
   console.log(i);
