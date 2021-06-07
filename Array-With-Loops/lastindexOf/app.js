38a66a5776b9f80bdff44ad7c658fc920e0457af
naima-shk <naimashaikh550@gmail.com>
2021-06-07 16:56:39 +0500

    'Array-with-function-implemented-JS-challenge-task-7'

---
+++ Array-With-Loops/lastindexOf/app.js
@@ -0,0 +1,4 @@
+//Use the lastIndexOf function to find a ‘Cricket’ member. Start from the 3rd index and print its index.
+let games=['hockey', 'cricket','football', 'badminton', 'ludo'];
+let b=games.lastIndexOf('cricket',3);
+console.log(b);
