3faa7e3889b06e5dba38e41908b2ca255b47898e
naima-shk <naimashaikh550@gmail.com>
2021-06-08 23:31:52 +0500

    'Utility-methods-implemented-JS-challenge-8'

---
+++ JS-utility-Methods/array-objects/app.js
@@ -0,0 +1,25 @@
+let usersData = [
+  {
+    name: "Saeed",
+
+    lastName: "Ahmad",
+
+    school: "UoT",
+
+    profession: "engineer",
+  },
+
+  {
+    name: "Ali",
+
+    lastName: "Khan",
+
+    school: "TUM",
+
+    profession: "researcher",
+  },
+];
+Object.keys(usersData).map(function(key, index) {
+    usersData[key];
+  });
+  console.log(usersData);
