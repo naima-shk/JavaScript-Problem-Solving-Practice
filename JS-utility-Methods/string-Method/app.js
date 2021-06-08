d069c476ca807c7f1e7928169aee22348771a966
naima-shk <naimashaikh550@gmail.com>
2021-06-08 17:32:26 +0500

    'Utility-methods-implemented-JS-challenge-8'

---
+++ JS-utility-Methods/string-Method/app.js
@@ -0,0 +1,10 @@
+/* 
+1-Use .length property to print length of a string cityName = ‘Tokyo’
+2-Find the index of ‘o’ character in the given string using indexOf method
+3-Use search method to locate ‘o’ in the given string and notice the difference between indexOf and search methods
+
+*/
+let cityName='Tokyo';
+//et b= cityName.length
+let c= cityName.indexOf('k',1);
+console.log(c);
