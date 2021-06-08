55f940318dba79000207d616a160b1c79ecc3280
naima-shk <naimashaikh550@gmail.com>
2021-06-08 16:46:53 +0500

    'Utility-methods-implemented-JS-challenge-8'

---
+++ JS-utility-Methods/Math-Method/app.js
@@ -0,0 +1,6 @@
+//Use ceil and floor methods to round off a numeric variable height = 7.534564
+let height1 =Math.ceil(7.534564);
+let height2 =Math.floor(7.534564);
+
+console.log(height1);
+console.log(height2);
