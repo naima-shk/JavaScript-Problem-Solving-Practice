4c5210682ffbcb335409d60c6301c3cc4e23504b
naima-shk <naimashaikh550@gmail.com>
2021-06-10 16:45:59 +0500

    'JS-challenge-task-9'

---
+++ Event-Listener-Form/app.js
@@ -0,0 +1,7 @@
+let form =document.getElementById('form');
+form.addEventListener('submit', function(event){
+    event.preventDefault()
+
+    let username =document.getElementById('username').Value;
+     let password = document.getElementById('password').Value;
+})
