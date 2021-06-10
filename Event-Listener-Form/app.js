6cd827d7a3b6dcff6147bf13199bd457433fa980
naima-shk <naimashaikh550@gmail.com>
2021-06-10 19:49:25 +0500

    'JS-challenge-task-9'

--- Event-Listener-Form/app.js
+++ Event-Listener-Form/app.js
@@ -1,7 +1,27 @@
-let form =document.getElementById('form');
-form.addEventListener('submit', function(event){
-    event.preventDefault()
 
-    let username =document.getElementById('username').Value;
-     let password = document.getElementById('password').Value;
-})
+  
+function submitFunction() {
+   let  login = document
+    .getElementById("login")
+    .addEventListener("submit", submitFunction, true);
+  let email = document.getElementById("email");
+  let password = document.getElementById("password");
+
+ alert("form submitted");
+  let  emailValue = email.value;
+	let  passwordValue = password.value; 
+  if (emailValue === "") {
+    alert("Email cannot be blank");
+  } else if (!isEmail(email)) {
+    alert("Not a valid email");
+  } else {
+    alert("email is valid");
+  }
+
+  if (passwordValue.length <= 8) {
+    console.log(alert("Password cannot be blank"));
+  } else {
+    console.log(alert("password is filled"));
+  }
+  console.log(alert("login succesfully"));
+}
