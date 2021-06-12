378bc370c480f2045801d328aeb5617dd6066435
naima-shk <naimashaikh550@gmail.com>
2021-06-12 15:44:58 +0500

    'form-validation-using-regex'

--- Event-Listener-Form/app.js
+++ Event-Listener-Form/app.js
@@ -17,3 +17,28 @@ function validation(){
   }
  
 }
+
+/* function submitFunction() {
+  login = document
+    .getElementById("#login")
+    .addEventListener("submit", submitFunction, true);
+  let email = document.getElementById("#email");
+  let password = document.getElementById("#password");
+  console.log(alert("form submitted"));
+  /*let  emailValue = email.value;
+    let  passwordValue = password.value;
+  if (email === "") {
+    alert("Email cannot be blank");
+  } else if (!isEmail(email)) {
+    alert("Not a valid email");
+  } else {
+    alert("email is valid");
+  }
+  if (password.length <= 8) {
+    console.log(alert("Password cannot be blank"));
+  } else {
+    console.log(alert("password is filled"));
+  }
+  console.log(alert("login succesfully"));
+}
+*/
